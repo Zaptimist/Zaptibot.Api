7cc0f4723ffebdf66294396797f907a55699cf61
Johannes Deden <johannies7@gmail.com>
2025-01-03 19:09:20 +0100

    Update Program.cs with Custom Exception Handler

---
+++ Zaptibot.Api/Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zaptibot.Api.Exceptions;
+
+internal sealed class GlobalExceptionHandler(IProblemDetailsService problemDetailsService) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        httpContext.Response.StatusCode = exception switch
+        {
+            ApplicationException => StatusCodes.Status400BadRequest,
+            ArgumentException => StatusCodes.Status400BadRequest,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        Activity? activity = httpContext.Features.Get<IHttpActivityFeature>()?.Activity;
+        
+        return await problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+        {
+            HttpContext = httpContext,
+            Exception = exception,
+            ProblemDetails = new ProblemDetails
+            {
+                Title = "An error occurred",
+                Type = exception.GetType().Name,
+                Status = httpContext.Response.StatusCode,
+                Detail = exception.Message,
+                Instance = $"{httpContext.Request.Method} {httpContext.Request.Path}",
+                Extensions = new Dictionary<string, object?>
+                {
+                    { "activityId", activity?.Id },
+                    { "traceId", httpContext.TraceIdentifier },
+                }
+            },
+        });
+    }
+}
--- Zaptibot.Api/Program.cs
+++ Zaptibot.Api/Program.cs
@@ -1,7 +1,16 @@
+using Zaptibot.Api.Exceptions;
+using Zaptibot.Api.Extensions;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services.AddProblemDetails();
+    builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
+    // Custom extension method
+    builder.Services.AddServices(builder.Configuration);
+    
+    builder.Services.AddControllers();
 }
 
 WebApplication app = builder.Build();
@@ -14,12 +23,20 @@ WebApplication app = builder.Build();
             // Improves developments speed. You don't have to press the "Try it out" button
             options.EnableTryItOutByDefault();
         });
+        
+        // NOTE: Create Users table when the application starts
+        await app.CreateUsersTableAsync();
+        await app.CreateSoundsTableAsync();
     }
 
     if (app.Environment.IsProduction())
     {
         app.UseHttpsRedirection();
     }
+    
+    app.MapControllers();
+    
+    app.UseExceptionHandler();
 }
 
 app.Run();
